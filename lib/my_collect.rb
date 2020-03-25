f905fa8ba8e01ef5508014b3ee76d41132c4ac79
Diane Crame <dcrame@icloud.com>
2020-03-25 18:17:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,15 +2,12 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    yield(array[i])
-    collection << array[i]
+    collection << yield(array[i])
     i += 1
   end
-  collection
+ collection
 end
 
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
+my_collect(array) {|name| name.split(' ').first}
